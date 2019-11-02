ea19351a3315dd5bea57d6be74704631a54bb6e1
hyunwoo Do <hyunwoodo@hyunwooui-MacBookPro.local>
2019-11-02 22:12:03 +0900

    ready to start project

--- app.js
+++ app.js
@@ -47,9 +47,6 @@ app.use(function(err, req, res, next) {
   res.render('error')
 })
 
-// db port setup(!!)
-// mongoose.connect('mongodb://localhost:27017/breeze')
-
 mongoose.connect('mongodb://'+process.env.DB_USER+':'+process.env.DB_PWD+'@'+process.env.DB_HOST+'/breeze?authSource=admin', {
   useNewUrlParser: true
 })
--- routes/contents.js
+++ routes/contents.js
@@ -6,39 +6,7 @@ const _ = require('lodash')
 // ------------------------ intropage route ----------------------------//
 
 router.get('/', function(req, res) {
-  Page.find({},(err,page)=>{
-    let gallerypaths = page[0].contents.content.galleries[0].images 
-    res.render('intro', {gallerypaths: gallerypaths})     
-  })
-})
-
-router.get('/home', function(req, res) {
-  Page.find({},(err,page)=>{
-    let gallerypaths = page[0].contents.content.galleries[0].images
-   
-    // arrangy by index
-    gallerypaths =  _.sortBy(gallerypaths,'index')
-
-    res.render('intro', {gallerypaths: gallerypaths})     
-  })
-})
-
-// ------------------------ about route ----------------------------//
-
-router.get('/about', function(req, res) {
-  Page.find({},(err,page)=>{
-    let headerimagePath = page[1].contents.content.headerimagePath
-    res.render('about', {headerimagePath: headerimagePath})     
-  })
-})
-
-// ------------------------ portfolio route ------------------------//
-
-router.get('/portfolio', function(req, res) {
-  Page.find({},(err,page)=>{
-    let gallerypaths = page[2].contents.content.galleries
-    res.render('portfolio', {gallerypaths: gallerypaths})  
-  })
+  res.render('intro')
 })
 
 module.exports = router;
